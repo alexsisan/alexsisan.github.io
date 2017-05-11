8e003230cf5a5d58143707582f6e230b1cc5ce53
alexsisan <sisan@live.co.uk>
2017-05-11 17:07:12 +0100

    New site

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
 var pkg = require('./package.json');
 
 // Set the banner content
@@ -18,7 +19,9 @@ var banner = ['/*!\n',
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-    return gulp.src('less/new-age.less')
+    var f = filter(['*', '!mixins.less', '!variables.less']);
+    return gulp.src('less/*.less')
+        .pipe(f)
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
@@ -29,7 +32,7 @@ gulp.task('less', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/new-age.css')
+    return gulp.src('css/freelancer.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
@@ -40,7 +43,7 @@ gulp.task('minify-css', ['less'], function() {
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('js/new-age.js')
+    return gulp.src('js/freelancer.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
@@ -50,17 +53,13 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy lib libraries from /node_modules into /lib
+// Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('lib/bootstrap'))
+        .pipe(gulp.dest('vendor/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('lib/jquery'))
-
-    gulp.src(['node_modules/simple-line-icons/*/*'])
-        .pipe(gulp.dest('lib/simple-line-icons'))
-
+        .pipe(gulp.dest('vendor/jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -70,7 +69,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('lib/font-awesome'))
+        .pipe(gulp.dest('vendor/font-awesome'))
 })
 
 // Run everything
